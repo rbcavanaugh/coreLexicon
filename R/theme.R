4d062416f432ad46e48b69f7102f87949dbba722
Rob Cavanaugh <rob.cavanaugh@pitt.edu>
2021-08-11 10:46:24 -0400

    last before change

--- R/theme.R
+++ R/theme.R
@@ -4,8 +4,8 @@
 minimal_theme <- function(){
   bslib::bs_theme(
         bootswatch = "default",
-         base_font = bslib::font_google("Open Sans"),
-         heading_font = bslib::font_google("Open Sans"),
+         #base_font = bslib::font_google("Open Sans"),
+         #heading_font = bslib::font_google("Open Sans"),
          version = "4",
          `enable-rounded` = T,
          `enable-transitions` = F,
--- inst/shiny-apps/coreLexicon/ui.R
+++ inst/shiny-apps/coreLexicon/ui.R
@@ -13,7 +13,17 @@ shinyUI(
                    footer_div()
                ),
                
-               theme = minimal_theme(),
+               theme = bslib::bs_theme(
+                 bootswatch = "default",
+                 #base_font = bslib::font_google("Open Sans"),
+                 #heading_font = bslib::font_google("Open Sans"),
+                 version = "4",
+                 `enable-rounded` = T,
+                 `enable-transitions` = F,
+                 primary = "#1665AC",
+                 "navbar-bg" = "#e9ecef"
+               ),
+                 #minimal_theme(),
                
                ############################ Instructions ############################## 
                tabPanel(value = "intro", title = "Intro", 
