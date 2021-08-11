fae580783c1c1abd0fa941b4b53973d63a52b5cf
Rob Cavanaugh <rob.cavanaugh@pitt.edu>
2021-08-11 10:59:38 -0400

    boom

--- R/run_app.R
+++ R/run_app.R
@@ -2,16 +2,7 @@
 #' Run Core Lexicon App
 #' @description Function to locally run the core lexicon app
 #' @export
-#' @import shiny bslib dplyr tidyr ggplot2 patchwork waiter truncnorm pkgload markdown
-#' @importFrom tibble tibble
-#' @importFrom scales label_percent
-#' @importFrom shinyjs enable disable show hide hidden useShinyjs
-#' @importFrom DT renderDT DTOutput
-#' @importFrom htmlwidgets JS
-#' @importFrom textstem lemmatize_words
-#' @importFrom tidytext unnest_tokens
-#' 
-#'
+#' @import shiny 
 #' @examples
 #' \dontrun{
 #' runCoreLex()
