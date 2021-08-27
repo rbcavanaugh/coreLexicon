e648d31d0501875616b7f3de695356b6ef0cc69c
Rob Cavanaugh <rob.cavanaugh@pitt.edu>
2021-08-26 22:21:52 -0400

    shinyapps.io

--- R/app_ui.R
+++ R/app_ui.R
@@ -58,7 +58,7 @@ golem_add_external_resources <- function(){
     favicon(),
     bundle_resources(
       path = app_sys('app/www'),
-      app_title = 'cl'
+      app_title = 'coreLexicon'
     )
     # Add here other external resources
     # for example, you can add shinyalert::useShinyalert() 
